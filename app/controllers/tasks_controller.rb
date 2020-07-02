44fdc0ae50c2ac4750c0a9786d36b6021481d5da
taise.yuuki <syuki120470@icloud.com>
2020-07-02 10:41:14 +0000

    tesks revenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show,:update,:destroy]
   def index
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -58,7 +59,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+      redirect_to tasks_url
     end
   end
   
