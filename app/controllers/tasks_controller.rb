b81d083f49e3d5c9e7682d9c70be5502a291e458
taise.yuuki <syuki120470@icloud.com>
2020-07-02 13:30:53 +0000

    tasks revenge2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :correct_user, only: [:show,:update,:destroy]
+  before_action :require_user_logged_in, only: [:index,:edit,:show]
+  before_action :correct_user, only: [:show,:edit,:update,:destroy]
   def index
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
  delete 'logout', to: 'sessions#destroy'
  
  get 'signup', to: 'users#new'
- resources :users, only: [:index, :show, :new, :create]
+ resources :users,only:[:new,:create]
  
  root to: 'tasks#index'
  
  resources :tasks
+ 
 end
